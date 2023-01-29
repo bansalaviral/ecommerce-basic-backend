4abe56e733f72876c5d473d2dbc02e96005c8d46
Aviral Bansal <nsitaviral@gmail.com>
2023-01-29 23:21:09 +0530

    updated Item model

--- models/item.js
+++ models/item.js
@@ -1,15 +1,20 @@
 const mongoose = require("mongoose");
 
-const ItemSchema = mongoose.Schema({
-  id: {
-    type: String,
-    required: [true, "Please mention id"],
+const ItemSchema = mongoose.Schema(
+  {
+    id: {
+      type: String,
+      required: [true, "Please mention id"],
+    },
+    qty: {
+      type: String,
+      required: [true, "Please mention quantity"],
+    },
   },
-  qty: {
-    type: String,
-    required: [true, "Please mention quantity"],
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Item = mongoose.model("Item", ItemSchema);
 module.exports = { ItemSchema, Item };
