1ced2080b18024becafe84150f531c0bd5217580
Aviral Bansal <nsitaviral@gmail.com>
2023-03-02 02:34:50 +0530

    added stripe and order route in server.js

--- server.js
+++ server.js
@@ -2,15 +2,28 @@ require("dotenv").config();
 const express = require("express");
 const productRoutes = require("./routes/productRoutes");
 const userRoutes = require("./routes/userRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const stripeRoutes = require("./routes/stripeRoutes");
 const connectDB = require("./config/db");
 
 connectDB();
 
 const app = express();
 
-app.use(express.json());
+app.use(
+  express.json({
+    // We need the raw body to verify webhook signatures.
+    // Let's compute it only when hitting the Stripe webhook endpoint.
+    verify: function (req, res, buf) {
+      if (req.originalUrl.startsWith("/api/stripe/webhook")) {
+        req.rawBody = buf.toString();
+      }
+    },
+  })
+);
 
 const cors = require("cors");
+
 app.use(cors());
 
 app.get("/", (req, res) => {
@@ -19,6 +32,8 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/stripe", stripeRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
